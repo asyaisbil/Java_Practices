f986b465b8c7b59493175769c37a33a2848aaeba
asyaisbil <asyaisbill@gmail.com>
2025-08-15 14:23:05 +0300

    Practice_1

---
+++ src/Practice_1/main_2.java
@@ -0,0 +1,9 @@
+//strNumber değişkenini integer bir değere cevirip number isminde bir değişkene atayınız.
+
+public class main_2 {
+    public static void main(String[] args) {
+        String strNumber = "5100";
+        int number = Integer.parseInt(strNumber);
+        System.out.println(number);
+    }
+}
